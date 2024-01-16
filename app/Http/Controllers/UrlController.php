30f65d9dbb133f8c8466c01e4f341cf22627bd4d
Niyi Omotoso <omoniyiomotoso@gmail.com>
2024-01-16 22:50:04 +0100

    add url controller logic

---
+++ app/Http/Controllers/UrlController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\UrlService;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
+class UrlController extends BaseController
+{
+    private UrlService $urlService;
+
+    public function __construct(UrlService $urlService)
+    {
+        $this->urlService = $urlService;
+
+    }
+
+    public function add(Request $request)
+    {
+        $rules = [
+            "url" => "required|url",
+        ];
+
+        $messages = [];
+        $this->validate($request, $rules, $messages);
+        $url = $request->get('url');
+        $alias = $request->get('alias');
+        $customPath = $request->get('customPath');
+
+
+        return $this->urlService->add(compact('url', 'alias', 'customPath'));
+    }
+}
