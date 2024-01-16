0e8b178b0364e19c4987a0a6194bc2d39c492d04
Niyi Omotoso <omoniyiomotoso@gmail.com>
2024-01-16 23:09:28 +0100

    add utility function

---
+++ app/Utility/CharacterGenerator.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Utility;
+
+class CharacterGenerator
+{
+    public static function generateRandomString($length = 6): string
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $randomString = '';
+
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, strlen($characters) - 1)];
+        }
+
+        return $randomString;
+    }
+}
