e477a271ecda86ced5ae543230926c1b30a581ec
Niyi Omotoso <omoniyiomotoso@gmail.com>
2024-01-25 10:26:57 +0100

    add build steps

--- deploy/staging/run.sh
+++ deploy/staging/run.sh
@@ -1,18 +1,21 @@
 set -e
 
-#in /var/www/smky-core/base
+cd /var/www/smky-core/base
 
-#laravel commands
+#lumen commands
 composer install --no-dev
-ls -la
+#composer update
+cp ../config/.env .env
 #php artisan migrate --force
 #php artisan db:seed --force
 php artisan cache:clear
 ####
 
-sudo rsync -a --exclude=live/ ./ live/
-sudo rm -r !(live)
-
+cd ../
+sudo rm -r backup
+sudo mv live backup
+mv base live
+# sudo mkdir base
 sudo chown -R www-data:www-data live
 sudo chmod -R 777 live/storage
 echo "done deploying"
