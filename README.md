d5b56a18b0e8103a720f263165acc1b69a2a198a
Niyi Omotoso <omoniyiomotoso@gmail.com>
2024-01-25 01:24:45 +0100

    add build steps

---
+++ README.md
@@ -0,0 +1,9 @@
+bytr.me Bytar
+
+webxy.me
+webxy.co
+
+https://medium.com/@bjnandi/how-to-deploy-laravel-application-to-ec2-instance-using-github-action-automatically-ssh-deploy-e198b0a3136d
+
+https://medium.com/@przyczynski/deploying-laravel-application-on-aws-ec2-using-github-actions-c83663f5c0eb
+
--- deploy/staging/run.sh
+++ deploy/staging/run.sh
@@ -1,22 +1,18 @@
 set -e
 
-#cd /var/www/smky-core/base
+#in /var/www/smky-core/base
 
 #laravel commands
 composer install --no-dev
-cd ../
 ls -la
-cp config/.env   .env
-cd base
 #php artisan migrate --force
 #php artisan db:seed --force
 php artisan cache:clear
 ####
 
-cd ../
-sudo rm -r backup
-sudo mv live backup
-mv base live
+sudo rsync -a --exclude=live/ ./ live/
+sudo rm -r !(live)
+
 sudo chown -R www-data:www-data live
 sudo chmod -R 777 live/storage
 echo "done deploying"
