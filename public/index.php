2bf472f3789aca93f7851c924cb9aa7a79a921f2
Niyi Omotoso <omoniyiomotoso@gmail.com>
2024-01-29 10:10:06 +0100

    test buy

--- public/index.php
+++ public/index.php
@@ -1,55 +1,23 @@
 <?php
+/**
+ * =======================================================================================
+ *                           GemFramework (c) GemPixel                                     
+ * ---------------------------------------------------------------------------------------
+ *  This software is packaged with an exclusive framework as such distribution
+ *  or modification of this framework is not allowed before prior consent from
+ *  GemPixel. If you find that this framework is packaged in a software not distributed 
+ *  by GemPixel or authorized parties, you must not use this software and contact gempixel
+ *  at https://gempixel.com/contact to inform them of this misuse.
+ * =======================================================================================
+ *
+ * @package GemPixel\Premium-URL-Shortener
+ * @author GemPixel (https://gempixel.com) 
+ * @license https://gempixel.com/licenses
+ * @link https://gempixel.com  
+ */
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
+  if (version_compare(phpversion(), '7.4', '<')) die('PHP 7.4 is required to run this software. Please upgrade your PHP version.');
 
-define('LARAVEL_START', microtime(true));
+	include(dirname(dirname(__FILE__))."/app/core.php");
 
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that any pre-rendered content can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
-
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
-
-require __DIR__.'/../vendor/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+  Gem::Bootstrap();
