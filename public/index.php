f43ce6db4e1544f94b518715794d711f19a20dd6
Niyi Omotoso <omoniyiomotoso@gmail.com>
2024-03-25 15:32:31 +0100

    add google tag manager

--- public/index.php
+++ public/index.php
@@ -1,13 +1,4 @@
-<!--<script async src="https://www.googletagmanager.com/gtag/js?id=AW-16506377365">-->
-<!--</script>-->
-<!--<script>-->
-<!--    window.dataLayer = window.dataLayer || [];-->
-<!--    function gtag(){dataLayer.push(arguments);}-->
-<!--    gtag('js', new Date());-->
-<!---->
-<!--    gtag('config', 'AW-16506377365');-->
-<!--</script>-->
- <?php
+<?php
 /**
  * =======================================================================================
  *                           GemFramework (c) GemPixel                                     
